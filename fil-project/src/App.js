a1b8215a77371c51b5318bc0441472a1b2022eb7
이름 <awaji0829@gmail.com>
2021-03-21 15:27:54 +0900

    fitst commit

---
+++ fil-project/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const App = () => {
+  return <Box>페스티벌 레고</Box>;
+};
+
+export default App;
+
+const Box = styled.div`
+  width: 100%;
+  background-color: pink;
+`;
---
+++ fil-project/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import GlobalStyles from './styles/GlobalStyles';
+import App from './App';
+
+ReactDOM.render(
+  <>
+    <GlobalStyles />
+    <App />
+  </>,
+  document.getElementById('root')
+);
